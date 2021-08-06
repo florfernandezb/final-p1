111c4cb6baaa03a1865db41ef00576ce6cc1b0ec
Florencia Fernandez Bugna <florenciafernandezb@hotmail.com>
2021-08-06 00:50:35 -0300

    version funcionando

---
+++ carrito.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function infoCarrito() {
+    producto
+    productoId,
+    cantidad,
+    total,
+    totalItems
+
+    this.getIdProducto = function() {
+        return productoId;
+    }
+
+    this.setIdProducto
+}
---
+++ producto.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function Producto(id = 0, nombre = "", descripcion = "", precio = 0, imagen = "") {
+    this.id = id;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.imagen = imagen;
+
+    this.setCantidad = function(cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    this.getCantidad = function(cantidad) {
+        return this.cantidad;
+    }
+}
